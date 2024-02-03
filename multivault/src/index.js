9d93ece981405a9b7d7e6a107dde97d40b76fb11
MrRyushi <john_patrick_marcellana@dlsu.edu.ph>
2024-02-03 23:58:40 +0800

    message

--- multivault/src/index.js
+++ multivault/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/index.css'
-import '../src/output.css'
 
 // Test
-import Register from './pages/noauth/Register/Register.jsx';
 import Login from './pages/noauth/Login/Login.jsx';
 
 
