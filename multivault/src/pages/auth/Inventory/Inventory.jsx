76e5fa50094060f2b2acb0dfa56a7be29ab4a09a
Gregorio John Marc A <122092610+1Cealus@users.noreply.github.com>
2024-03-11 22:41:07 +0800

    Prioritization Feature

--- multivault/src/pages/auth/Inventory/Inventory.jsx
+++ multivault/src/pages/auth/Inventory/Inventory.jsx
@@ -1,10 +1,14 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import './Inventory.css';
 import './InventoryHead.css';
 import './InventorySide.css';
 import { Pagination, Dropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { collection, doc, getDoc, addDoc, getDocs, updateDoc, query, where, onSnapshot} from "firebase/firestore";
+import { auth, db } from '../../../firebase/firebase';
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 import EditItem from './EditItem.jsx';
 
 const Inventory = ({ items, onShowAddItem, onShowEditItem, activeSubPage, sortItemsByMinQuantity, sortItemsByMaxQuantity, sortItemsAsc, sortItemsDesc, sortExpAsc, sortExpDesc, setItemToEdit, onToggleSelectItem, selectedItems }) => {
@@ -12,12 +16,46 @@ const Inventory = ({ items, onShowAddItem, onShowEditItem, activeSubPage, sortIt
   const [itemsPerPage] = useState(8);
   const [sortBy, setSortBy] = useState('Name');
   const [filterBy, setFilterBy] = useState('All');
+  const [prioritizedItems, setPrioritizedItems] = useState({});
+  const itemsCollectionRef = collection(db, `users/${auth.currentUser?.uid}/items`);
+  const [allItems, setAllItems] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(itemsCollectionRef, (snapshot) => {
+      const itemsData = [];
+      snapshot.forEach(doc => {
+        itemsData.push({ id: doc.id, ...doc.data() });
+      });
+      setAllItems(itemsData);
+    });
+  
+    return () => unsubscribe();
+  }, []);  
+
+
+  useEffect(() => {
+    const fetchPrioritizedItems = async () => {
+      try {
+        setSortBy('Priority');
+        const querySnapshot = await getDocs(itemsCollectionRef);
+        const prioritizedItemsData = {};
+        querySnapshot.forEach(doc => {
+          prioritizedItemsData[doc.id] = doc.data().prioritized || false;
+        });
+        setPrioritizedItems(prioritizedItemsData);
+      } catch (error) {
+        console.error('Error fetching prioritized items:', error);
+      }
+    };
+  
+    fetchPrioritizedItems();
+  }, []);
 
   const filteredItems = useMemo(() => {
-    let tempItems = items;
+    let tempItems = allItems;
 
     if (activeSubPage === 'low-stock') {
-      tempItems = items.filter(item => item.quantity <= 50);
+      tempItems = allItems.filter(item => item.quantity <= 50);
     }
 
     if (filterBy === 'Perishable') {
@@ -26,19 +64,74 @@ const Inventory = ({ items, onShowAddItem, onShowEditItem, activeSubPage, sortIt
       tempItems = tempItems.filter(item => item.status === 'Non-Perishable');
     }
 
+    if (filterBy === "Prioritized") {
+      tempItems = tempItems.filter((item) => prioritizedItems[item.id]);
+    }
+    
+
     switch (sortBy) {
       case 'Name':
-        return tempItems.sort((a, b) => a.name.localeCompare(b.name));
+        return [...tempItems].sort((a, b) => a.name.localeCompare(b.name));
       case 'Quantity':
-        return tempItems.sort((a, b) => a.quantity - b.quantity);
+        return [...tempItems].sort((a, b) => Number(a.quantity) - Number(b.quantity));
+      case 'Priority':
+        return [...tempItems].sort((a, b) => {
+          if (prioritizedItems[a.id] && !prioritizedItems[b.id]) {
+            return -1;
+          } else if (!prioritizedItems[a.id] && prioritizedItems[b.id]) {
+            return 1;
+          } else {
+            return a.name.localeCompare(b.name);
+          }
+        });
       default:
         return tempItems;
     }
-  }, [items, activeSubPage, sortBy, filterBy]);
+     
+  }, [allItems, activeSubPage, sortBy, filterBy, prioritizedItems]);
 
-
+  const togglePrioritize = async (itemId) => {
+    console.log(itemId);
+    try {
+      const itemRef = doc(itemsCollectionRef, itemId);
+      const itemSnap = await getDoc(itemRef);
+  
+      if (!itemSnap.exists()) {
+        console.log('No matching document.');
+        return null;
+      }
+  
+      const itemData = itemSnap.data();
+  
+      const updatedItemData = {
+        ...itemData,
+        prioritized: !itemData.prioritized
+      };
+  
+      await updateDoc(itemRef, updatedItemData);
+  
+      console.log('Prioritization status updated successfully.');
+  
+      setPrioritizedItems(prevPrioritizedItems => ({
+        ...prevPrioritizedItems,
+        [itemId]: updatedItemData.prioritized
+      }));
+  
+      setSortBy(prevSortBy => prevSortBy === 'Priority' ? null : prevSortBy);
+    } catch (error) {
+      console.error('Error updating prioritization status:', error);
+  
+      setPrioritizedItems(prevPrioritizedItems => ({
+        ...prevPrioritizedItems,
+        [itemId]: !prevPrioritizedItems[itemId]
+      }));
+    }
+  };
+  
+  
+  
+  
   const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
-
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
@@ -94,6 +187,7 @@ const Inventory = ({ items, onShowAddItem, onShowEditItem, activeSubPage, sortIt
                 <Dropdown.Item onClick={() => { setSortBy('Quantity Descending'); setDropdownOpen(false); sortItemsByMaxQuantity()}}>Quantity Descending</Dropdown.Item>
                 <Dropdown.Item onClick={() => { setSortBy('Expiration Date Ascending'); setDropdownOpen(false); sortExpAsc()}}>Expiration Ascending</Dropdown.Item>
                 <Dropdown.Item onClick={() => { setSortBy('Expiration Date Descending'); setDropdownOpen(false); sortExpDesc()}}>Expiration Descending</Dropdown.Item>
+                <Dropdown.Item onClick={() => { setSortBy('Favorited'); setDropdownOpen(false); }}>Favorited</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
             <div className="filter-buttons">
@@ -115,6 +209,15 @@ const Inventory = ({ items, onShowAddItem, onShowEditItem, activeSubPage, sortIt
 											onChange={() => onToggleSelectItem(item.index)}
 										/>
 										<button className="item-edit-button" onClick={() => {setItemToEdit(item); onShowEditItem();}}></button>
+                    <button
+                      className="prioritize-button"
+                      onClick={() => {
+                        console.log('Button clicked. Item id:', item.id);
+                        togglePrioritize(item.id);
+                      }}
+                    >
+                      {prioritizedItems[item.id] ? <StarIcon /> : <StarBorderIcon />}
+                    </button>
                   </div>
                   <img src={item.imageurl} alt={item.name || "Default placeholder"}/>
                   <div className="item-name">
