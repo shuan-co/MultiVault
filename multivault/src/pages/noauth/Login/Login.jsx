5750dceaf49cf1bfc10e14f523e5cdf7b5482ec5
MrRyushi <jptmarcellana@gmail.com>
2024-03-31 02:54:30 +0800

    forgot password

--- multivault/src/pages/noauth/Login/Login.jsx
+++ multivault/src/pages/noauth/Login/Login.jsx
@@ -2,16 +2,17 @@
 import React, { useState, useEffect } from 'react'
 import loginBg from './loginBg.jpg'
 import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { onAuthStateChanged, signInWithEmailAndPassword, getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { auth, db } from '../../../firebase/firebase';
 
 export default function Login() {
 	const navigate = useNavigate();
 	const [activeButton, setActiveButton] = useState('User');
-	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [error, setError] = useState('');
 	const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [email, setEmail] = useState('')
 
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -68,8 +69,74 @@ export default function Login() {
         }
     }
 
+    const resetPassword = (email) => {
+        const auth = getAuth();
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            alert('Password reset email sent!')
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode + " " + errorMessage)
+        });
+    }
+
+    const handleForgotPasswordSubmit = () => {
+        setShowModal(false)
+        resetPassword(email)
+    }
+
 	return (
 		<div style={{ backgroundImage: `url(${loginBg})` }} className='flex items-center justify-center h-screen'>
+            <div>
+            {showModal ? (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold text-black">
+                                        Reset Password
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none"> × </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto text-black space-y-3">
+                                    <h1 className="text-xl">Enter your email:</h1>
+                                    <input type="email" className="rounded-lg bg-slate-200 w-96 p-2" onChange={(e) => setEmail(e.target.value)}/>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="bg-yellow-500 text-white active:bg-yellow-500 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => handleForgotPasswordSubmit()}
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+                ) : null}
+            </div>
 			<div className='w-screen'>
 				<h1 className='text-center text-white text-8xl inter font-extrabold mb-5'>MultiVault</h1>
 				<div className='w-11/12 sm:w-2/3 md:w-2/3 lg:w-1/2 xl:w-1/3 h-96 mx-auto bg-slate-200 rounded-xl p-5 border'>
@@ -104,7 +171,7 @@ export default function Login() {
                                     className='text-white bg-amber-500 rounded-md p-2 hover:bg-amber-400
                                                mx-auto w-3/4 block'>Sign in</button>
 							<div className='text-center space-x-4 text-slate-400 border w-full'>
-								<a className='text-xs hover:underline underline-offset-4'>Forgot password</a>
+								<a className='text-xs hover:underline underline-offset-4' onClick={() => setShowModal(true)}>Forgot password</a>
 								<a onClick={() => navigate('/register')} className='text-xs hover:underline underline-offset-4'>Create an account</a>
 							</div>
 						</div>
